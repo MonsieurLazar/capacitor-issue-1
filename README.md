150c0378160248e3b518dc3a14d06dde6cf96c36
lazar <64156137+MonsieurLazar@users.noreply.github.com>
2023-05-29 20:28:45 +0300

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# open capacitor app in android studio
+```bash
+cd my-app
+npx cap open android
+```
+
+## checking bug
---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+
+    import { Capacitor } from "@capacitor/core";
+	import { StatusBar, Style } from '@capacitor/status-bar';
+
+    import { onMount } from 'svelte';
+
+	onMount(async () => {
+		if (Capacitor.isPluginAvailable('StatusBar') && Capacitor.getPlatform() !== 'web'){
+            StatusBar.setBackgroundColor({
+                color: "#ff715b"
+            })
+		}
+	});
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<style>
+    :global(body){
+        background-color: #ff715b;
+        color: #ffffff;
+    }
+</style>
